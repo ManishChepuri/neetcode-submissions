40570bf7ac1a2f761ee847401045b4ec1bdf7ee3
Manish Chepuri <98428664+ManishChepuri@users.noreply.github.com>
2026-09-09 17:32:06 -0400

    Add: search-for-word - submission-8

---
+++ Data Structures & Algorithms/search-for-word/submission-8.py
@@ -0,0 +1,61 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        word_idx = 0
+        board_rows = len(board)
+        board_cols = len(board[0])
+        visited: set(Tuple) = set()
+
+        def _backtrack(row: int, col: int, word_idx: int):
+            if word_idx >= len(word):
+                return True
+
+            found = False
+
+            if row - 1 >= 0 and (row - 1, col) not in visited and board[row - 1][col] == word[word_idx]: # Top letter
+                # print(f"row: {row - 1}, col:{col}")
+                # print(f"word_idx: {word_idx}")
+                # print(f"letter: {board[row - 1][col]}\n")
+                visited.add((row, col))
+                if _backtrack(row - 1, col, word_idx + 1):
+                    found = True
+                else:
+                    visited.remove((row, col))
+            if row + 1 < board_rows and (row + 1, col) not in visited and board[row + 1][col] == word[word_idx]: # Bottom letter
+                # print(f"row: {row + 1}, col:{col}")
+                # print(f"word_idx: {word_idx}")
+                # print(f"letter: {board[row + 1][col]}\n")
+                visited.add((row, col))
+                if _backtrack(row + 1, col, word_idx + 1):
+                    found = True
+                else:
+                    visited.remove((row, col))
+            if col - 1 >= 0 and (row, col - 1) not in visited and board[row][col - 1] == word[word_idx]: # Left letter
+                # print(f"row: {row}, col:{col - 1}")
+                # print(f"word_idx: {word_idx}")
+                # print(f"letter: {board[row][col - 1]}\n")
+                visited.add((row, col))
+                if _backtrack(row, col - 1, word_idx + 1):
+                    found = True
+                else:
+                    visited.remove((row, col))
+            if col + 1 < board_cols and (row, col + 1) not in visited and board[row][col + 1] == word[word_idx]: # Right letter
+                # print(f"row: {row}, col:{col + 1}")
+                # print(f"word_idx: {word_idx}")
+                # print(f"letter: {board[row][col + 1]}\n")
+                visited.add((row, col))
+                if _backtrack(row, col + 1, word_idx + 1):
+                    found = True
+                else:
+                    visited.remove((row, col))
+
+            return found
+
+        for i in range(0, len(board)):
+            for j in range(0, len(board[i])):
+                if board[i][j] == word[0]:
+                    # print(f"First Letter: {board[i][j]}\n")
+                    word_idx = 1
+                    if _backtrack(i, j, word_idx):
+                        return True
+
+        return False
